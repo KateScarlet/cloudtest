188e6cd6a8a54fe67da11c7a483b31b79c8e4b08
PhosphorLin <sbt3357@gmail.com>
2022-04-20 13:30:21 +0800

    password

--- login/cloudlogin.py
+++ login/cloudlogin.py
@@ -1,17 +1,19 @@
 import pyotp
 from locate import cloudlocate
 import cryptocode
-key=""
+
+key = "enter password here" #输入加密密钥
+
 cloudTotp = {
     "pre": "Yo+TgPLk8ynYuyxtsy0kCr5s1Zp80uYtgyhGWmaRi9A=*Ucsjb+B8B2avn8hTxkG9eA==*HnPBC2lWEdkZIH348wKHgg==*Cpv5B6VR3rNeaD7YlUiGmg==",
     "prd": "fgwEmc+dmzC6pUNJIWlgoah7t7u1+pvyXs+6g7afKo8=*Qypei3v94DUNgxqtq3w9mw==*BTTMIcZFuEXJtb+sYo6BXg==*04BGS1346VSmC6ejc5G62Q==",
     "gfprd": "6omGaluVOChp2gH6kW9sv9SnZ6mdIV/Nh/VtHdupDiY=*nkp2BTLRWahi5FeDU61HgQ==*jKvly8TQfWyv/HW3IYoNpg==*Iuv7JqVWiWF/PXkwsIZE6w==",
     "gfpre": "zAKqt2j/Kg+EaKNALUaac3UAwLAJu48xr2+ZHuXVZUQ=*3apFdnEpGR+Y4fAO/EerdA==*S/H9M+bH9ap4yl3DzqCbRQ==*lZCc2hDs5K99l5XA4iDdGA=="
 }
-totpPre = pyotp.TOTP(cryptocode.decrypt(cloudTotp["pre"],key)).now
-totpPrd = pyotp.TOTP(cryptocode.decrypt(cloudTotp["prd"],key)).now
-totpGfPrd = pyotp.TOTP(cryptocode.decrypt(cloudTotp["gfprd"],key)).now
-totpGfPre = pyotp.TOTP(cryptocode.decrypt(cloudTotp["gfpre"],key)).now
+totpPre = pyotp.TOTP(cryptocode.decrypt(cloudTotp["pre"], key)).now
+totpPrd = pyotp.TOTP(cryptocode.decrypt(cloudTotp["prd"], key)).now
+totpGfPrd = pyotp.TOTP(cryptocode.decrypt(cloudTotp["gfprd"], key)).now
+totpGfPre = pyotp.TOTP(cryptocode.decrypt(cloudTotp["gfpre"], key)).now
 
 cloudUrl = {
     "pre": "https://login.cloud.zj.gov.cn/auth/realms/master/protocol/openid-connect/auth?client_id=ascm_yibao&response_type=code&scope=super&redirect_uri=https://asc.res.zj.hsip.gov.cn/oauth2/authCallback",
@@ -33,7 +35,8 @@ cloudUser = {
 def loginPre(page):  # 预发环境
     page.goto(cloudUrl["pre"])
     page.locator("[placeholder=\"请输入用户名\"]").fill(cloudUser["pre"][0])
-    page.locator("[placeholder=\"请输入密码\"]").fill(cryptocode.decrypt(cloudUser["pre"][1],key))
+    page.locator("[placeholder=\"请输入密码\"]").fill(
+        cryptocode.decrypt(cloudUser["pre"][1], key))
     with page.expect_navigation():
         page.locator("text=登 录").click()
     page.locator("input[name=\"totp\"]").fill(totpPre())
@@ -47,7 +50,8 @@ def loginPre(page):  # 预发环境
 def loginPrd(page):  # 生产环境
     page.goto(cloudUrl["prd"])
     page.locator("[placeholder=\"请输入用户名\"]").fill(cloudUser["prd"][0])
-    page.locator("[placeholder=\"请输入密码\"]").fill(cryptocode.decrypt(cloudUser["prd"][1],key))
+    page.locator("[placeholder=\"请输入密码\"]").fill(
+        cryptocode.decrypt(cloudUser["prd"][1], key))
     with page.expect_navigation():
         page.locator("text=登 录").click()
     page.locator("input[name=\"totp\"]").fill(totpPrd())
@@ -61,7 +65,8 @@ def loginPrd(page):  # 生产环境
 def loginTest(page):  # 测试环境
     page.goto(cloudUrl["test"])
     page.locator("[placeholder=\"账户名\"]").fill(cloudUser["test"][0])
-    page.locator("[placeholder=\"密码\"]").fill(cryptocode.decrypt(cloudUser["test"][1],key))
+    page.locator("[placeholder=\"密码\"]").fill(
+        cryptocode.decrypt(cloudUser["test"][1], key))
     with page.expect_navigation():
         page.locator("button:has-text(\"登录\")").click()
     page.wait_for_load_state(state='load')
@@ -72,7 +77,8 @@ def loginTest(page):  # 测试环境
 def loginGfPrd(page):  # 公服生产
     page.goto(cloudUrl["gfprd"])
     page.locator("[placeholder=\"请输入用户名\"]").fill(cloudUser["gfprd"][0])
-    page.locator("[placeholder=\"请输入密码\"]").fill(cryptocode.decrypt(cloudUser["gfprd"][1],key))
+    page.locator("[placeholder=\"请输入密码\"]").fill(
+        cryptocode.decrypt(cloudUser["gfprd"][1], key))
     with page.expect_navigation():
         page.locator("text=登 录").click()
     page.locator("input[name=\"totp\"]").fill(totpGfPrd())
@@ -86,7 +92,8 @@ def loginGfPrd(page):  # 公服生产
 def loginGfPre(page):  # 公服预发
     page.goto(cloudUrl["gfpre"])
     page.locator("[placeholder=\"请输入用户名\"]").fill(cloudUser["gfpre"][0])
-    page.locator("[placeholder=\"请输入密码\"]").fill(cryptocode.decrypt(cloudUser["pgfpre"][1],key))
+    page.locator("[placeholder=\"请输入密码\"]").fill(
+        cryptocode.decrypt(cloudUser["pgfpre"][1], key))
     with page.expect_navigation():
         page.locator("text=登 录").click()
     page.locator("input[name=\"totp\"]").fill(totpGfPre())
